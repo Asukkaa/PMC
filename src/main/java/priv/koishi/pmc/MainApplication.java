0e7bfb97d178f476b73d3b81769c548cb9f80ef1
Asukkaa <76054083+Asukkaa@users.noreply.github.com>
2025-05-22 13:38:21 +0800

    设置全局本地化为中文

--- src/main/java/priv/koishi/pmc/MainApplication.java
+++ src/main/java/priv/koishi/pmc/MainApplication.java
@@ -279,6 +279,7 @@ public class MainApplication extends Application {
         }
         // 启动激活监听服务
         startActivationServer(port);
+        Locale.setDefault(Locale.CHINA);
         launch();
     }
 
