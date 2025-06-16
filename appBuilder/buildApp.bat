d2107d95c2dcbea5cc4ad79dff42a4d1a97c1fc3
Koishi <csolxqt@foxmail.com>
2025-06-16 17:34:03 +0800

    更改默认gc方式为zgc

--- appBuilder/buildApp.bat
+++ appBuilder/buildApp.bat
@@ -44,7 +44,7 @@ if exist "%appName%" (
 )
 
 :: 执行打包
-jpackage --name "%appName%" --type app-image -m "%appMainClass%" --runtime-image "%runtimeImage%" --icon "%appIcon%" --app-version "%appVersion%"
+jpackage --name "%appName%" --type app-image -m "%appMainClass%" --runtime-image "%runtimeImage%" --icon "%appIcon%" --app-version "%appVersion%" --java-options "-XX:+UseZGC"
 echo 已完成 jpackage 打包
 
 :: 生成压缩包后打开目录并选中生成的应用程序文件夹
