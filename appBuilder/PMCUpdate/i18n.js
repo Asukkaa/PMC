8e487be5eef3c411324ecbae8b34060865af6fab
Koishi <csolxqt@foxmail.com>
2025-08-14 12:38:39 +0800

    更新服务端脚本

--- appBuilder/PMCUpdate/i18n.js
+++ appBuilder/PMCUpdate/i18n.js
@@ -1,28 +1,25 @@
 module.exports = {
     "zh-CN": {
-        whatsNew: `版本3.1.1:
-        1.修复在 macOS 下更新应用后可能无法正常立刻重启的问题
-        2.更新对话框将会显示新版本的发布时间
-        3.下载更新时将会显示已下载的文件大小
-        4.优化界面布局`,
+        whatsNew: `版本3.1.2:
+        1.选择文件相关功能支持批量选择
+        2.修复了一些bug
+        3.优化界面布局`,
         invalidJson: "无效的JSON请求体",
         invalidOS: "无效的操作系统参数"
     },
     "zh-TW": {
-        whatsNew: `版本3.1.1:
-        1.修復在macOS下更新應用後可能無法正常立刻重啓的問題
-        2.更新對話方塊將會顯示新版本的發佈時間
-        3.下載更新時將會顯示已下載的文件大小
-        4.優化介面佈局`,
+        whatsNew: `版本3.1.2:
+        1.選擇檔案相關功能支持批量選擇
+        2.修復了一些bug
+        3.優化介面佈局`,
         invalidJson: "無效的JSON請求體",
         invalidOS: "無效的作業系統參數"
     },
     "en": {
-        whatsNew: `Version 3.1.1:
-        1.Fixed an issue where updating apps on macOS may not immediately restart properly
-        2.The update dialog box will display the release time of the new version
-        3.When downloading updates, the downloaded file size will be displayed
-        4.Optimize interface layout`,
+        whatsNew: `Version 3.1.2:
+        1.Select file related functions and support batch selection
+        2.Fixed some bugs
+        3.Optimize interface layout`,
         invalidJson: "Invalid JSON request body",
         invalidOS: "Invalid OS parameter"
     }
