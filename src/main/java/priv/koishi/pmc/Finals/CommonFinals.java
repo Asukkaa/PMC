d331fbab414330cbbe0c55e22a1ddeb40f3daca8
Koishi <csolxqt@foxmail.com>
2025-11-06 16:13:31 +0800

    修复执行自动任务时，程序主界面任务进度信息字体颜色没有恢复为原本颜色的问题

--- src/main/java/priv/koishi/pmc/Finals/CommonFinals.java
+++ src/main/java/priv/koishi/pmc/Finals/CommonFinals.java
@@ -36,7 +36,7 @@ public class CommonFinals {
     /**
      * 程序构建日期
      */
-    public static final String buildDate = "2025.11.05";
+    public static final String buildDate = "2025.11.06";
 
     /**
      * 文件后缀名：bat
