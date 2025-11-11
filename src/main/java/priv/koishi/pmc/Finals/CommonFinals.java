bf67238db8f28484152ecbba935b3d344e902575
Koishi <csolxqt@foxmail.com>
2025-11-11 14:35:32 +0800

    升级依赖版本

--- src/main/java/priv/koishi/pmc/Finals/CommonFinals.java
+++ src/main/java/priv/koishi/pmc/Finals/CommonFinals.java
@@ -36,7 +36,7 @@ public class CommonFinals {
     /**
      * 程序构建日期
      */
-    public static final String buildDate = "2025.11.10";
+    public static final String buildDate = "2025.11.11";
 
     /**
      * 文件后缀名：bat
