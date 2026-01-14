7cedc2a40a06977a2f924741d10ab5a0ff8378b8
Koishi <csolxqt@foxmail.com>
2026-01-14 17:51:49 +0800

    更新服务端脚本

--- appBuilder/PMCUpdate/i18n.js
+++ appBuilder/PMCUpdate/i18n.js
@@ -2,9 +2,9 @@ module.exports = {
     "zh-CN": {
         versionPrefix: "版本",
         whatsNewItems: `
-        修复信息浮窗位置显示可能不正确的问题
-        优化图像识别范围设置功能
-        完善部分文本描述
+        新增键盘自动化功能，详情页可设置键盘输入与组合键输入，录制功能支持键鼠同时录制
+        新增全局快捷键功能，取消、录制、运行都可单独设置快捷键
+        修复若干 bug
         优化界面布局`,
         invalidJson: "无效的JSON请求体",
         invalidOS: "无效的操作系统参数"
@@ -12,9 +12,9 @@ module.exports = {
     "zh-TW": {
         versionPrefix: "版本",
         whatsNewItems: `
-        修正資訊浮動視窗位置可能顯示不正確的問題
-        優化圖像識別範圍設定功能
-        完善部分文字描述
+        新增鍵盤自動化功能，詳情頁可設置鍵盤輸入與組合鍵輸入，錄製功能支援鍵鼠同時錄製
+        新增全域快捷鍵功能，取消、錄製、執行都可單獨設置快捷鍵
+        修復若干 bug
         優化介面佈局`,
         invalidJson: "無效的JSON請求體",
         invalidOS: "無效的作業系統參數"
@@ -22,9 +22,9 @@ module.exports = {
     "en": {
         versionPrefix: "Version",
         whatsNewItems: `
-        Fix the issue where the position of the information floating window may display incorrectly
-        Optimize the image recognition scope setting function
-        Improve some text descriptions
+        Added keyboard automation functionality; the details page now supports setting keyboard input and key combination input, and the recording feature allows simultaneous keyboard and mouse recording
+        Added global hotkey functionality, allowing separate hotkey settings for cancel, record, and run operations
+        Fixed several bugs
         Optimize interface layout`,
         invalidJson: "Invalid JSON request body",
         invalidOS: "Invalid OS parameter"
