109ca5f378d90903db324ddf66987df68d0db121
Koishi <csolxqt@foxmail.com>
2026-01-14 17:57:46 +0800

    更新服务端脚本

--- appBuilder/PMCUpdate/i18n.js
+++ appBuilder/PMCUpdate/i18n.js
@@ -4,6 +4,8 @@ module.exports = {
         whatsNewItems: `
         新增键盘自动化功能，详情页可设置键盘输入与组合键输入，录制功能支持键鼠同时录制
         新增全局快捷键功能，取消、录制、运行都可单独设置快捷键
+        自动操作步骤详情页标题栏也能设置显示实时鼠标位置，方便设置鼠标坐标
+        加强了自动流程导入导出功能，将会识别脚本版本，新版本的脚本无法在旧版本的 PMC 中打开，但新版本的 PMC 能够打开旧版本的脚本
         修复若干 bug
         优化界面布局`,
         invalidJson: "无效的JSON请求体",
@@ -14,6 +16,8 @@ module.exports = {
         whatsNewItems: `
         新增鍵盤自動化功能，詳情頁可設置鍵盤輸入與組合鍵輸入，錄製功能支援鍵鼠同時錄製
         新增全域快捷鍵功能，取消、錄製、執行都可單獨設置快捷鍵
+        操作步骤详情页的标题栏可设置显示即时滑鼠位置，便于设定滑鼠座标
+        强化了自动流程汇入汇出功能，将辨识脚本版本：新版本的脚本无法在旧版 PMC 中开启，但新版 PMC 可开启旧版脚本
         修復若干 bug
         優化介面佈局`,
         invalidJson: "無效的JSON請求體",
@@ -24,6 +28,8 @@ module.exports = {
         whatsNewItems: `
         Added keyboard automation functionality; the details page now supports setting keyboard input and key combination input, and the recording feature allows simultaneous keyboard and mouse recording
         Added global hotkey functionality, allowing separate hotkey settings for cancel, record, and run operations
+        The title bar of the operation step details page can now be set to display the real‑time mouse cursor position, making it easier to configure mouse coordinates
+        Enhanced the import/export function for automated workflows: it now detects script versions. Scripts from newer versions cannot be opened in older versions of PMC, while newer versions of PMC can open scripts from older versions
         Fixed several bugs
         Optimize interface layout`,
         invalidJson: "Invalid JSON request body",
