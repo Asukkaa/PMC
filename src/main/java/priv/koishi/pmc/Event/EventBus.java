5d7f26ce8da60d7f634aacb921733d313e62ec56
Koishi <csolxqt@foxmail.com>
2026-01-26 17:09:11 +0800

    完善事件总线，使其在新增事件后能够正确触发回调

--- src/main/java/priv/koishi/pmc/Event/EventBus.java
+++ src/main/java/priv/koishi/pmc/Event/EventBus.java
@@ -23,14 +23,28 @@ public class EventBus {
     private static final Map<Class<?>, List<Consumer<?>>> listeners = new ConcurrentHashMap<>();
 
     /**
+     * 历史事件
+     */
+    private static final Map<Class<?>, Object> lastEvents = new ConcurrentHashMap<>();
+
+    /**
      * 订阅事件
      *
      * @param eventType 事件类型
      * @param listener  事件监听器
      * @param <T>       事件类型
      */
-    public static <T> void subscribe(Class<T> eventType, Consumer<T> listener) {
+    public static <T> void subscribe(Class<T> eventType, Consumer<? super T> listener) {
         listeners.computeIfAbsent(eventType, _ -> new CopyOnWriteArrayList<>()).add(listener);
+        // 如果有历史事件，立即触发
+        Object lastEvent = lastEvents.get(eventType);
+        if (lastEvent != null) {
+            Platform.runLater(() -> {
+                @SuppressWarnings("unchecked")
+                T event = (T) lastEvent;
+                listener.accept(event);
+            });
+        }
     }
 
     /**
@@ -41,6 +55,8 @@ public class EventBus {
      */
     @SuppressWarnings("unchecked")
     public static <T> void publish(T event) {
+        // 存储最后的事件
+        lastEvents.put(event.getClass(), event);
         List<Consumer<?>> eventListeners = listeners.get(event.getClass());
         if (eventListeners != null) {
             Platform.runLater(() -> {
