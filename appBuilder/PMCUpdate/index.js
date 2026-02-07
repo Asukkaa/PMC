ab47f4441bb25165445f3e024b906fe579ce1845
Koishi <csolxqt@foxmail.com>
2026-02-07 18:16:13 +0800

    更新服务端脚本

--- appBuilder/PMCUpdate/index.js
+++ appBuilder/PMCUpdate/index.js
@@ -21,9 +21,9 @@ exports.main = async (event) => {
     // 客户端当前版本
     const clientVersion = requestBody.version;
     // 服务端最新版本
-    const serverVersion = "4.3.0";
+    const serverVersion = "4.3.1";
     // 构建日期
-    const buildDate = "2026.02.04";
+    const buildDate = "2026.02.07";
     // 获取更新信息
     const versionInfo = i18n[clientLang];
     // 拼接更新信息
