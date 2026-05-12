ac01038a38470a409e95b64416100c98fca345b0
Koishi <csolxqt@foxmail.com>
2026-05-12 18:24:03 +0800

    修复列表数据拖拽出列表后会报错的问题

--- src/main/java/priv/koishi/pmc/Finals/CommonFinals.java
+++ src/main/java/priv/koishi/pmc/Finals/CommonFinals.java
@@ -50,7 +50,7 @@ public class CommonFinals {
     /**
      * 程序构建日期
      */
-    public static final String buildDate = "2026.05.08";
+    public static final String buildDate = "2026.05.12";
 
     /**
      * 文件后缀名：bat
