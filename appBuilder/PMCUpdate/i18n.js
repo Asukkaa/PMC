35e53b01e94e114c5d30c8a4dd09f6ca8ba4ccd9
Koishi <csolxqt@foxmail.com>
2026-07-13 18:16:54 +0800

    更新版本信息

--- appBuilder/PMCUpdate/i18n.js
+++ appBuilder/PMCUpdate/i18n.js
@@ -5,7 +5,8 @@ module.exports = {
         修复列表单元格编辑时数据可能填充异常的问题
         优化应用关闭时配置文件保存逻辑
         修复鼠标悬停提示在窗口右边缘显示不下时不停闪烁的问题
-        部分文件相关页面新增文件变化自动刷新功能`,
+        部分文件相关页面新增文件变化自动刷新功能
+        自动流程运行脚本功能新增环境检测相关逻辑，可在流程详情页查看相关环境是否符合运行条件`,
         invalidJson: "无效的 JSON 请求体",
         invalidOS: "无效的操作系统参数"
     },
@@ -15,7 +16,8 @@ module.exports = {
         修復清單儲存格編輯時資料可能填入異常的問題
         最佳化應用程式關閉時設定檔儲存邏輯
         修復滑鼠懸停提示在視窗右邊緣顯示不完全時不停閃爍的問題
-        部分檔案相關頁面新增檔案變更自動重新整理功能`,
+        部分檔案相關頁面新增檔案變更自動重新整理功能
+        自動流程執行指令碼功能新增環境檢測相關邏輯，可在流程詳細頁面檢視相關環境是否符合執行條件`,
         invalidJson: "無效的 JSON 請求體",
         invalidOS: "無效的作業系統參數"
     },
@@ -25,7 +27,8 @@ module.exports = {
         Fixed an issue where data might be incorrectly filled when editing list cells
         Optimized the configuration file saving logic when the application is closed
         Fixed an issue where tooltips on mouse hover would flicker when they couldn't be fully displayed at the right edge of the window
-        Added auto-refresh functionality for file changes on some file-related pages`,
+        Added auto-refresh functionality for file changes on some file-related pages
+        The script execution feature in automation workflows now includes environment detection logic. You can check whether the relevant environment meets the execution conditions on the workflow details page`,
         invalidJson: "Invalid JSON request body",
         invalidOS: "Invalid OS parameter"
     }
