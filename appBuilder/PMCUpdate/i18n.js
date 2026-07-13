d35dc3f4ba559cabda23791506e88295f011e53f
Koishi <csolxqt@foxmail.com>
2026-07-13 18:19:48 +0800

    更新版本信息

--- appBuilder/PMCUpdate/i18n.js
+++ appBuilder/PMCUpdate/i18n.js
@@ -2,6 +2,7 @@ module.exports = {
     "zh-CN": {
         versionPrefix: "版本",
         whatsNewItems: `
+        新增应用自动检测更新频率设置功能，可在【关于】页面选择应用自动检测更新频率
         修复列表单元格编辑时数据可能填充异常的问题
         优化应用关闭时配置文件保存逻辑
         修复鼠标悬停提示在窗口右边缘显示不下时不停闪烁的问题
@@ -13,6 +14,7 @@ module.exports = {
     "zh-TW": {
         versionPrefix: "版本",
         whatsNewItems: `
+        新增應用程式自動檢查更新頻率的設定功能，可在【關於】頁面中選擇自動檢查更新的頻率
         修復清單儲存格編輯時資料可能填入異常的問題
         最佳化應用程式關閉時設定檔儲存邏輯
         修復滑鼠懸停提示在視窗右邊緣顯示不完全時不停閃爍的問題
@@ -24,6 +26,7 @@ module.exports = {
     "en": {
         versionPrefix: "Version",
         whatsNewItems: `
+        Added a feature to set the frequency of automatic update checks. You can select the update check frequency on the 【About】 page
         Fixed an issue where data might be incorrectly filled when editing list cells
         Optimized the configuration file saving logic when the application is closed
         Fixed an issue where tooltips on mouse hover would flicker when they couldn't be fully displayed at the right edge of the window
