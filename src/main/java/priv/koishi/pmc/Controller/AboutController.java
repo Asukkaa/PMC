73b768f0b50500d909276e6702e1c9f308d70544
Koishi <csolxqt@foxmail.com>
2026-08-24 12:40:30 +0800

    优化代码

--- src/main/java/priv/koishi/pmc/Controller/AboutController.java
+++ src/main/java/priv/koishi/pmc/Controller/AboutController.java
@@ -478,8 +478,7 @@ public class AboutController extends RootController {
         checkDate_Abt.setText("");
         TaskBean<?> taskBean = new TaskBean<>();
         taskBean.setMessageLabel(checkMessage_Abt)
-                .setDisableNodes(disableNodes)
-                .setBindingMessageLabel(true);
+                .setDisableNodes(disableNodes);
         Task<CheckUpdateBean> task = checkLatestVersion();
         taskBean.setWorkingTask(task)
                 .setOnFailed(_ -> updateCheckDate(Color.RED))
